2dafec95712f73a6eedc31313d78b5e91d224a25
Nkosinathi MNCUBE <nmncube@c4r10s8.wethinkcode.co.za>
2019-11-20 17:18:30 +0200

    Working on image page navigation

--- Camagru/grid.php
+++ Camagru/grid.php
@@ -2,6 +2,11 @@
 session_start();
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 include "likes.php";
+
+function add()
+{
+    return (1);
+}
 ?>
 <html>
 <head>
@@ -24,6 +29,11 @@ include "likes.php";
         </ul>
         <div >
             <h1><u>Public Gallery Section</u></h1>
+            <form action = "" method = "post" >
+            <button name = "Prev">Prev</button>
+            <button name = "Next">Next</button>
+            </form>
+            
         </div>
     </nav>
 <section class="container">
@@ -32,7 +42,18 @@ include "likes.php";
         include "config/database.php";
         include_once "config/connection.php";
         $pdo = DB_Connection( $DB_DSN, $DB_NAME, $DB_USER, $DB_PASSWORD);
-       $sql = 'SELECT * FROM images ORDER BY created DESC';
+        //$num = 0;
+        if (isset($_POST['Next']) && $num )
+        {
+            $num = $num + 1;
+        }
+        else
+        {
+            $num = 0;
+        }
+       $sql = 'SELECT * FROM images ORDER BY created DESC LIMIT '.$num.',2' ;
+       //$sql = 'SELECT * FROM images ORDER BY created DESC;
+       //DESC LIMIT 1,2' ;
        $stmt = $pdo->prepare($sql);
        $stmt->execute([$_SESSION['verf_no']]);
        $post = $stmt->fetchAll();
--- Camagru/main.php
+++ Camagru/main.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['verf_no']))
 ?>
 <html>
     <head>
-    <link rel = "stylesheet" href="css/.css">
+    <link rel = "stylesheet" type = "text/css" href="style2/.css">
     </head>
 
     <body>   
